6741dc7a81ae013e91069e0af57e11e83768775b
osyniaeva <dn100488rol@gmail.com>
2017-06-14 14:00:52 +0300

    fetching of skills from back-end

--- src/main/webapp/src/api/Api.js
+++ src/main/webapp/src/api/Api.js
@@ -1,2 +1,38 @@
-export function getAllSills(){
+import fetch from "isomorphic-fetch";
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    return Promise.reject(response);
+  }
 }
+
+function handleError(response) {
+  if (response instanceof Response) {
+    return new Promise((resolve, reject) => {
+      response.json().then(error => {
+        reject(new Error(error.message));
+      });
+    });
+  }
+  return Promise.reject(response);
+}
+
+
+const doRequest = (baseUrl, path, reqType, data) =>
+  fetch(`${baseUrl}${HOME}/${path}`, {
+    method: reqType,
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+
+export const  getAllSkills = (baseUrl, path, reqType) =>
+  doRequest(baseUrl, path, reqType)
+    .then(checkStatus)
+    .then(response => response.json())
+    .catch(handleError);
--- src/main/webapp/src/containers/App.js
+++ src/main/webapp/src/containers/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import '../App.css';
-import {connect} from 'react-redux';
+import React, {Component} from "react";
+import "../App.css";
+import {connect} from "react-redux";
+import {getAllSkills} from "../actions/ActionCreators";
+import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
+import {Checkbox, Col} from "react-bootstrap";
 
 
 class App extends Component {
+
+  const allSkills = this.props.allSkills;
+
+  componentWillMount() {
+    this.props.dispatch(getAllSkills());
+  }
+
   render() {
-    return (
-      <div className="App">
-      </div>
-    );
+    return <div className = "App">
+      </div>;
   }
 }
 
 function mapStateToProps(state) {
-    return {
-        allSkills: state.allSills
-    }
+  return {
+    allSkills: state.allSills
+  }
 }
 
 export default connect(mapStateToProps)(App);
